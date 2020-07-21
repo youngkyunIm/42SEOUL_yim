d8e9750085514db6566c6e0fd7cde02cb4d9487d
Youngkyoon Im <yim@c3r1s6.42seoul.kr>
2020-07-21 21:22:04 +0900

    07

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: yim <yim@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/14 22:35:09 by yim               #+#    #+#             */
-/*   Updated: 2020/07/19 18:18:20 by yim              ###   ########.fr       */
+/*   Updated: 2020/07/21 20:55:53 by yim              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,17 +27,19 @@ void	square(int *n)
 	}
 }
 
-void	print(int c, int nb)
+void	print(int c, int nb, int ex)
 {
-	int m;
-	int n;
-	char mm;
+	int		m;
+	int		n;
+	char	mm;
 
 	while (c >= 0)
 	{
 		n = c;
 		if (nb < 10)
 		{
+			if (ex == 1)
+				nb++;
 			mm = nb % 10 + 48;
 			write(1, &mm, 1);
 			return ;
@@ -51,14 +53,37 @@ void	print(int c, int nb)
 	}
 }
 
+void	pass(int nb)
+{
+	int		c;
+	int		n;
+	int		ex;
+
+	ex = 0;
+	c = -1;
+	if (nb < 0)
+	{
+		if (nb == -2147483648)
+		{
+			nb = -2147483647;
+			ex = 1;
+		}
+		nb = -nb;
+	}
+	n = nb;
+	while (n > 0)
+	{
+		c++;
+		n /= 10;
+	}
+	print(c, nb, ex);
+}
+
 void	ft_putnbr(int nb)
 {
-	char minus;
-	char zero;
-	int c;
-	int n;
+	char	minus;
+	char	zero;
 
-	c = -1;
 	minus = '-';
 	zero = '0';
 	if (nb == 0)
@@ -69,13 +94,6 @@ void	ft_putnbr(int nb)
 	if (nb < 0)
 	{
 		write(1, &minus, 1);
-		nb = -nb;
 	}
-	n = nb;
-	while (n > 0)
-	{
-		c++;
-		n /= 10;
-	}
-	print(c, nb);
+	pass(nb);
 }
--- C00/ex08/ft_print_combn.c
+++ C00/ex08/ft_print_combn.c
@@ -3,47 +3,77 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_combn.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yim <marvin@42.fr>                         +#+  +:+       +#+        */
+/*   By: yim <yim@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/07/14 20:22:10 by yim               #+#    #+#             */
-/*   Updated: 2020/07/14 21:12:44 by yim              ###   ########.fr       */
+/*   Created: 2020/07/20 11:41:32 by yim               #+#    #+#             */
+/*   Updated: 2020/07/21 20:08:27 by yim              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 
+char	g_arr[10];
+char	g_copy[10];
 
-#include<unistd.h>
+void	ft_print_combn(int n);
+void	combi(int num, int n, int c);
 
-void combi(int size, int n, int c)
+void	print(int c)
 {
-	int i=0;
-	if(n==1)
-	{
-	printf("----1----\n");
-		while(i<c)
-		{
-			write(1,&cparr[i],1);
-		}
-			write(1,',',1);
-			write(1,' ',1);
-		return;
-	}
-	//return;
-}
+	int		i;
 
-void ft_print_combn(int n)
-{
-	char a[10];
-	
-	arr = (char*)malloc(size*sizeof(int));
-	cparr = (char*)malloc(size*sizeof(int));
-	while(i<=size)
+	i = 0;
+	while (i < c)
 	{
-		arr[i]=i+48;
+		write(1, &g_copy[i], 1);
 		i++;
 	}
-	combi(size,n,c);
-	free(arr);
-	free(cparr);
-	return;
+	return ;
+}
+
+void	combi(int num, int n, int c)
+{
+	char	space;
+	char	com;
+
+	com = ',';
+	space = ' ';
+	if (n == 0)
+	{
+		print(c);
+		if (num == 0)
+			return ;
+		write(1, &com, 1);
+		write(1, &space, 1);
+		return ;
+	}
+	if (num < n || num == 0)
+	{
+		return ;
+	}
+	else
+	{
+		g_copy[n - 1] = g_arr[num - 1];
+		combi(num - 1, n - 1, c);
+		combi(num - 1, n, c);
+	}
+}
+
+void	ft_print_combn(int n)
+{
+	int		i;
+	int		c;
+	int		num;
+
+	num = 10;
+	c = n;
+	i = 0;
+	while (i <= num)
+	{
+		g_arr[i] = i + 48;
+		g_copy[i] = 48;
+		i++;
+	}
+	combi(num, n, c);
+	return ;
 }
